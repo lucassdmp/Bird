83f44f648ca61d1baa0992f1120de4963426ef20
Lucas <lucassdmp01@gmail.com>
2025-02-15 14:51:51 -0300

    added login

--- lib/bird_web/endpoint.ex
+++ lib/bird_web/endpoint.ex
@@ -50,5 +50,6 @@ defmodule BirdWeb.Endpoint do
   plug Plug.MethodOverride
   plug Plug.Head
   plug Plug.Session, @session_options
+  plug Pow.Plug.Session, otp_app: :bird
   plug BirdWeb.Router
 end
